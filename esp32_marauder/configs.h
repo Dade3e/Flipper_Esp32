087cbe0583b3c68a0a64a45e0ae516435291bab1
Davide Barchetta <dbarchetta@gheppio.hq.mquadro.net>
2023-07-03 13:56:56 +0200

    Aggiornato marauder a v0.10.8

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -6,7 +6,7 @@
 
   //Indicates that it must redirect the stream with the captured packets to serial (1)
   //If not defined, will write packages to SD card if supported
-  #define WRITE_PACKETS_SERIAL
+  //#define WRITE_PACKETS_SERIAL
   
   //#define MARAUDER_M5STICKC
   //#define MARAUDER_MINI
